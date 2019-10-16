d243ef7ed4b668f4b4eb370fe36df404a3807100
Honza Blažek <gimli@matfyz.cz>
2019-10-16 11:10:58 +0200

    fixes for batch run

--- src/models/igre_test_batch.py
+++ src/models/igre_test_batch.py
@@ -5,10 +5,10 @@ from copy import deepcopy
 import os
 
 
-OUT_FILENAME_FORMAT = "x{SHIFT_X}_y{SHIFT_Y}_modality_step{MODALITY_DIFF}_lr{LEARNING_RATE}_{CUSTOM}.result"
+OUT_FILENAME_FORMAT = "x{SHIFT_X}_y{SHIFT_Y}_modality_step{MODALITY_DIFF}_{CUSTOM}.result"
 
 
-def __create_batch(config, shift, custom_string):
+def __create_batch(config, shift, repeats):
     """ In principle we expect valid configuration for igre in batch config. But there is possible batch
     configuration in "batch" property. Each variable requires special care, therefore there will be an if
     block in this method for each batch configuration.
@@ -16,32 +16,21 @@ def __create_batch(config, shift, custom_string):
     template = deepcopy(config)
     del template["batch"]
     template["output"] = OUT_FILENAME_FORMAT.replace("{SHIFT_X}", str(shift[0]))\
-                                            .replace("{SHIFT_Y}", str(shift[1])) \
-                                            .replace("{CUSTOM}", custom_string)
-    batch = [template]
+                                            .replace("{SHIFT_Y}", str(shift[1]))
+    batch = []
     for param in config["batch"]["params"]:
-        if param == "output_dimension":
-            new_batch = []
-            for value in np.arange(config["batch"][param]["min"],
-                                   config["batch"][param]["max"],
-                                   config["batch"][param]["step"]):
-                for cfg in batch:
-                    new_cfg = deepcopy(cfg)
+        for repeat in range(repeats):
+            if param == "output_dimension":
+                for value in np.arange(config["batch"][param]["min"],
+                                       config["batch"][param]["max"],
+                                       config["batch"][param]["step"]):
+                    new_cfg = deepcopy(template)
                     new_cfg["output_dimensions"]["min"] = value
                     new_cfg["output_dimensions"]["max"] = value
                     new_cfg["output"] = new_cfg["output"]\
-                        .replace("{MODALITY_DIFF}", str(np.abs(new_cfg["input_dimensions"]["min"] - value)))
-                    new_batch.append(new_cfg)
-            batch = new_batch
-        elif param == "learning_rate":
-            new_batch = []
-            for value in config["batch"][param]:
-                for cfg in batch:
-                    new_cfg = deepcopy(cfg)
-                    new_cfg["train"]["optimizer"]["learning_rate"] = value
-                    new_cfg["output"] = new_cfg["output"].replace("{LEARNING_RATE}", str(value))
-                    new_batch.append(new_cfg)
-            batch = new_batch
+                        .replace("{MODALITY_DIFF}", str(value - new_cfg["input_dimensions"]["min"]))\
+                        .replace("{CUSTOM}", str(repeat))
+                    batch.append(new_cfg)
     return batch
 
 
@@ -79,16 +68,17 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "-s",
-        "--custom_string",
-        type=str,
-        default="no",
-        help="String to customize output filenames (e.g. repetition number)",
+        "--repeats",
+        type=int,
+        default=20,
+        help="Number of repeated computations with different random seed",
     )
     args = parser.parse_args()
     if not os.path.exists(args.batch_dir):
         os.makedirs(args.batch_dir)
     with open(args.config) as config_file:
-        batch_config = yaml.load(config_file)
-    batch = __create_batch(batch_config, (args.x_shift, args.y_shift), args.custom_string)
+        batch_config = yaml.load(config_file, Loader=yaml.FullLoader)
+    batch = __create_batch(batch_config, (args.x_shift, args.y_shift), args.repeats)
+    print("done")
     for run_conf in batch:
         igre_test(run_conf, (args.x_shift, args.y_shift), os.path.join(args.batch_dir, run_conf["output"]))
