6b81fcb7e0a9973260fac333522127d3d8988185
Tomas Karella <karella@utia.cas.cz>
2021-02-04 22:06:55 +0100

    CNN Phantom generating

--- src/data/phantom/materials.py
+++ src/data/phantom/materials.py
@@ -1,7 +1,12 @@
 # TODO: Add description and names
 import numpy as np
 import os
+import glob
 from enum import Enum, auto
+from scipy.stats import multivariate_normal
+import matplotlib.pyplot as plt
+import scipy.io as sio
+
 ALMA_NAME = 'alma'
 FIRENZE_NAME = 'firenze'
 # TODO: Solve more elegant way
@@ -20,3 +25,104 @@ def load_alma():
 def load_firenze():
     return np.load(MATERIALS_FILE)[FIRENZE_NAME]
 
+
+def extract_pigments(vis_wildcard,
+                     nir_wildcard,
+                     mask_path,
+                     mask_name,
+                     padding=6,
+                     max_value=255.0):
+    vis = glob.glob(vis_wildcard)
+    nir = glob.glob(nir_wildcard)
+    mask = sio.loadmat(mask_path)[mask_name]
+
+    layers = [*vis, *nir]
+    x, y = plt.imread(layers[0]).shape
+    z = len(layers)
+
+    raw_img = np.zeros((x, y, z))
+    for i, l in enumerate(layers):
+        raw_img[:, :, i] = plt.imread(l)
+
+    raw_img /= max_value
+
+    z_size = raw_img.shape[-1]
+    pigments = []
+
+    for mask_id in range(1, np.max(mask) + 1):
+        mask_multid = np.repeat(
+            np.expand_dims(
+                (mask == mask_id),
+                axis=-1),
+            repeats=z_size,
+            axis=-1
+        )
+
+        x_size = np.max(np.sum(mask_multid[:, :, 1], axis=0))
+        y_size = np.max(np.sum(mask_multid[:, :, 1], axis=1))
+        square = np.reshape(raw_img[mask_multid], (x_size, y_size, z_size))
+
+        no_draw = square[:, :int(square.shape[1] / 2) - padding, :]
+        draw = square[:, int(square.shape[1] / 2) + padding:, :]
+
+        pigments.append({'no_draw': no_draw,
+                         'draw': draw})
+
+    return pigments
+
+
+def get_pigment_dist(pigment, pad=1):
+    # MARK: returns distribution (pad * 2 + 1, pad * 2 + 1, z_size)
+    assert len(pigment.shape) == 3, f"Expected 3 dimensional image, but get {len(pigment.shape)}"
+    assert pad >= 1
+    z_size = pigment.shape[-1]
+    target_shape = (pad * 2 + 1, pad * 2 + 1, z_size)
+    # TODO: Do not waste memory, size known
+    samples = []
+    for i in range(pad, pigment.shape[0] - 1):
+        for j in range(pad, pigment.shape[1] - 1):
+            sample = pigment[i - pad:i + pad + 1, j - pad:j + pad + 1, :]
+            # Normalize from 0 to 1
+            assert sample.shape == target_shape
+            # Flatten as feature vector
+            samples.append(np.ravel(sample))
+    samples = np.array(samples)
+    covariance_matrix = np.cov(samples, rowvar=False)
+    mean_vector = np.mean(samples, axis=0)
+    assert mean_vector.shape == np.product(target_shape)
+    assert covariance_matrix.shape == (np.product(target_shape), np.product(target_shape))
+    # TODO: Is it okey to use 0 - 255
+    # TODO: Normality test ??
+    # TODO: pigment distribution class ???
+
+    return multivariate_normal(mean=mean_vector,
+                               cov=covariance_matrix,
+                               allow_singular=False)
+
+
+def create_cnn_phantom(pigments_distribution,
+                       used_materials,
+                       number_samples,
+                       underdrawing_coverage,
+                       sample_shape,
+                       seed=42):
+    no_draws = []
+    draws = []
+    clear_coverage = int(np.floor(number_samples * underdrawing_coverage))
+    correct_outliers = True
+
+    for i in used_materials:
+        clear_dist = pigments_distribution[i]['no_draw']
+        underdrawing_dist = pigments_distribution[i]['draw']
+        clear_sample = clear_dist.rvs(clear_coverage, random_state=seed)
+        clear_sample = np.reshape(clear_sample, (clear_coverage, *sample_shape))
+        underdrawing_sample = underdrawing_dist.rvs(number_samples - clear_coverage, random_state=seed)
+        underdrawing_sample = np.reshape(underdrawing_sample, (number_samples - clear_coverage, *sample_shape))
+        if correct_outliers:
+            clear_sample[clear_sample < 0] = 0
+            clear_sample[clear_sample > 1] = 1
+            underdrawing_sample[underdrawing_sample < 0] = 0
+            underdrawing_sample[underdrawing_sample > 1] = 1
+        draws.append(underdrawing_sample)
+        no_draws.append(clear_sample)
+    return {'draw': draws, 'no_draw': no_draws}
---
+++ src/data/phantom/tools.py
@@ -0,0 +1,94 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from src.models.ig_cnn import wavelength2rgb
+
+
+def _draw(pigments, image2color, cmap='viridis'):
+    fig, axs = plt.subplots(6,
+                            int(len(pigments) / 6),
+                            figsize=(20, 20))
+    for i, pigment in enumerate(pigments):
+        no_draw, draw = pigment['no_draw'], pigment['draw']
+        merged_pigment = np.concatenate((no_draw,
+                                         np.ones((draw.shape[0], 20, draw.shape[-1])),
+                                         draw),
+                                        axis=1)
+        axs[i % axs.shape[0], i // axs.shape[0]].imshow(
+            image2color(merged_pigment), cmap=cmap
+        )
+    return fig
+
+
+def draw_pigments_vis(pigments):
+    def _vis(img):
+        return wavelength2rgb(img[:, :, :16])
+    return _draw(pigments, _vis)
+
+
+def draw_pigments_nir(pigments, channel=25):
+    def _nir(img):
+        return img[:, :, channel]
+
+    return _draw(pigments, _nir, cmap='gray')
+
+
+def _compare_distribution_origin(pigments, pigments_dist, image2color, cmap='viridis'):
+    fig, axs = plt.subplots(30, 2, figsize=(10, 60))
+    for id, pig in enumerate(pigments):
+        (ax1, ax2) = axs[id]
+        im = []
+        for i in range(50):
+            l = []
+            for s in pigments_dist[id]['no_draw'].rvs(20):
+                l.append(np.reshape(s, (3, 3, 32)))
+            im.append(l)
+        im = np.reshape(np.array(im), (150, 60, 32))
+        im[im < 0] = 0
+        im[im > 1] = 1.0
+        generated = image2color(im)[:120, :50]
+        origin = image2color(pig['no_draw'])[:120, :50]
+        mean = image2color(np.ones((120, 50, 32)) * np.mean(pig['no_draw'], axis=(0, 1)))
+        comp1 = np.concatenate((generated, origin, mean), axis=1)
+        im = []
+        for i in range(50):
+            l = []
+            for s in pigments_dist[id]['draw'].rvs(20):
+                l.append(np.reshape(s, (3, 3, 32)))
+            im.append(l)
+        im = np.reshape(np.array(im), (150, 60, 32))
+        im[im < 0] = 0
+        im[im > 1] = 1.0
+        generated = image2color(im)[:120, :50]
+        origin = image2color(pig['draw'])[:120, :50]
+        mean = image2color(np.ones((120, 50, 32)) * np.mean(pig['draw'], axis=(0, 1)))
+        comp2 = np.concatenate((generated, origin, mean), axis=1)
+
+        ax1.imshow(comp1, cmap=cmap)
+        ax2.imshow(comp2, cmap=cmap)
+    return fig
+
+
+def compare_distribution_origin_vis(pigments, pigments_dist):
+    def _vis(img):
+        return wavelength2rgb(img[:, :, :16])
+    return _compare_distribution_origin(pigments, pigments_dist, _vis)
+
+
+def compare_distribution_origin_nir(pigments, pigments_dist, channel=25, cmap='gray'):
+    def _nir(img):
+        return img[:, :, channel]
+    return _compare_distribution_origin(pigments, pigments_dist, _nir, cmap)
+
+
+def show_cnn_phantom(cnn_phantoms, pad):
+    rows = []
+    for no_draw, draw in zip(cnn_phantoms['no_draw'], cnn_phantoms['draw']):
+        rows.append(np.concatenate((
+            np.reshape(no_draw,
+                       ((2*pad + 1) ** 2, no_draw.shape[0], no_draw.shape[-1])),
+            np.reshape(draw,
+                       ((2*pad + 1) ** 2, draw.shape[0], draw.shape[-1]))
+        ), axis=1))
+    fig = plt.figure()
+    plt.imshow(wavelength2rgb(np.concatenate(rows, axis=0)[:,:,:16]))
+    return fig
